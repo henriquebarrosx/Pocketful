08c23964a9c28cda1a46c34795c092bc963146b0
Henrique Barros <henriquebarrosx@gmail.com>
2024-05-16 22:21:34 -0300

    Create project from scratch

---
+++ src/main/java/com/pocketful/PocketfulApplication.java
@@ -0,0 +1,11 @@
+package com.pocketful;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocketfulApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PocketfulApplication.class, args);
+    }
+}
---
+++ src/test/java/com/pocketful/PocketfulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pocketful;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocketfulApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
