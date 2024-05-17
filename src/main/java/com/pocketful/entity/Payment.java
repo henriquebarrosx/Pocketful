f2b77b365d40239918fe3836a875a47019966e6f
Henrique Barros <henriquebarrosx@gmail.com>
2024-05-16 22:33:11 -0300

    Create payment entity

---
+++ src/main/java/com/pocketful/entity/Payment.java
@@ -0,0 +1,40 @@
+package com.pocketful.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Table
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Payment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    @Column(nullable = false)
+    private float amount;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column(nullable = false)
+    private Boolean payed;
+
+    @Column(nullable = false)
+    private Boolean isExpense;
+
+    @Column
+    private LocalDate deadlineAt;
+
+    @Column(nullable = false)
+    private LocalDate createdAt;
+
+    @Column(nullable = false)
+    private LocalDate updatedAt;
+}
