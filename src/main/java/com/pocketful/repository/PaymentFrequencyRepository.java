5e9260c4e11ca867d9cc0945cc391ac73892a32f
Henrique Barros <henriquebarrosx@gmail.com>
2024-05-16 22:42:31 -0300

    Create payment frequency repository

---
+++ src/main/java/com/pocketful/repository/PaymentFrequencyRepository.java
@@ -0,0 +1,6 @@
+package com.pocketful.repository;
+
+import com.pocketful.entity.PaymentFrequency;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PaymentFrequencyRepository extends JpaRepository<PaymentFrequency, Long> {}
