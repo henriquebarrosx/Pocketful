1c8bff34857859c76a023e00681b8b4f73840df3
Henrique Barros <henriquebarrosx@gmail.com>
2024-05-25 10:55:51 -0300

    Create custom conflict exception and global exception handler

---
+++ src/main/java/com/pocketful/exception/GlobalExceptionHandler.java
@@ -0,0 +1,19 @@
+package com.pocketful.exception;
+
+import com.pocketful.web.dto.exception.ExceptionDTO;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@Slf4j
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(ConflictException.class)
+    public ResponseEntity<ExceptionDTO> handleConflictException(Exception exception) {
+        log.error(exception.getMessage());
+        ExceptionDTO exceptionDTO = new ExceptionDTO(exception.getMessage());
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(exceptionDTO);
+    }
+}
