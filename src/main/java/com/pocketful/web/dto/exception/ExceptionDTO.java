ced1379c7dda7ece08dcd5929498903ced7bfb50
Henrique Barros <henriquebarrosx@gmail.com>
2024-05-25 11:05:46 -0300

    Create ExceptionDTO class

---
+++ src/main/java/com/pocketful/web/dto/exception/ExceptionDTO.java
@@ -0,0 +1,12 @@
+package com.pocketful.web.dto.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class ExceptionDTO {
+    private final String message;
+}
