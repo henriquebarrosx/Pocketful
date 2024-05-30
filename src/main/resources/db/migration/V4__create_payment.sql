ca9558534bbbe4e29b6f5d0fc4c17e9a0cac1e22
Henrique Barros <henriquebarrosx@gmail.com>
2024-05-30 15:41:41 -0300

    Add missing account foreign key into payment migration

--- src/main/resources/db/migration/V4__create_payment.sql
+++ src/main/resources/db/migration/V4__create_payment.sql
@@ -8,11 +8,13 @@ CREATE TABLE payments
     payed                   BOOLEAN      NOT NULL,
     is_expense              BOOLEAN      NOT NULL,
     deadline_at             date,
+    account_id              BIGINT       NOT NULL,
     payment_category_id     BIGINT       NOT NULL,
     payment_frequency_id    BIGINT       NOT NULL,
     created_at              TIMESTAMP    DEFAULT    CURRENT_TIMESTAMP,
     updated_at              TIMESTAMP    DEFAULT    CURRENT_TIMESTAMP,
     CONSTRAINT pk_payments PRIMARY KEY (id),
+    CONSTRAINT pk_accounts FOREIGN KEY (account_id) REFERENCES accounts (id),
     CONSTRAINT pk_payment_categories FOREIGN KEY (payment_category_id) REFERENCES payment_categories (id),
     CONSTRAINT pk_payment_frequencies FOREIGN KEY (payment_frequency_id) REFERENCES payment_frequencies (id)
 );
