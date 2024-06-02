cd2f2f1f072f1319bacbcec3ab6a42bdcb366979
Henrique Barros <henriquebarrosx@gmail.com>
2024-06-02 19:35:50 -0300

    Validate account registration

---
+++ src/test/java/com/pocketful/service/AccountServiceTest.java
@@ -0,0 +1,90 @@
+package com.pocketful.service;
+
+import com.pocketful.entity.Account;
+import com.pocketful.exception.ConflictException;
+import com.pocketful.repository.AccountRepository;
+import com.pocketful.web.dto.account.NewAccountDTO;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+class AccountServiceTest {
+    @InjectMocks
+    private AccountService accountService;
+
+    @Mock
+    private AccountRepository accountRepository;
+
+    @BeforeEach
+    public void setup() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    void shouldThrowErrorWhenRegistryAccountUsingExistentEmail() {
+        String EXISTENT_MAIL = "john.doe@mail.com";
+
+        NewAccountDTO newAccountDTO = NewAccountDTO.builder()
+            .name("John Doe")
+            .email(EXISTENT_MAIL)
+            .phoneNumber("5582988990011")
+            .build();
+
+        when(accountRepository.existsAccountByEmailOrPhoneNumber(EXISTENT_MAIL, newAccountDTO.getPhoneNumber()))
+            .thenReturn(true);
+
+        ConflictException exception = assertThrows(ConflictException.class, () -> accountService.create(newAccountDTO));
+        assertEquals(exception.getMessage(), "An account with this email or phone number already exists.");
+    }
+
+    @Test
+    void shouldThrowErrorWhenRegistryAccountUsingExistentPhoneNumber() {
+        String EXISTENT_PHONE_NUMBER = "5582988990011";
+
+        NewAccountDTO newAccountDTO = NewAccountDTO.builder()
+            .name("John Doe")
+            .phoneNumber(EXISTENT_PHONE_NUMBER)
+            .email("john.doe@mail.com")
+            .build();
+
+        when(accountRepository.existsAccountByEmailOrPhoneNumber(newAccountDTO.getEmail(), EXISTENT_PHONE_NUMBER))
+            .thenReturn(true);
+
+        ConflictException exception = assertThrows(ConflictException.class, () -> accountService.create(newAccountDTO));
+        assertEquals(exception.getMessage(), "An account with this email or phone number already exists.");
+    }
+
+    @Test
+    void shouldReturnAnAccountWhenRegistrySuccessfullyUsingNonExistentEmailOrPhoneNumber() {
+        Account actual = Account.builder()
+            .id(1L)
+            .name("John Doe")
+            .email("john.doe@mail.com")
+            .phoneNumber("5582988776655")
+            .build();
+
+        when(accountRepository.existsAccountByEmailOrPhoneNumber(any(String.class), any(String.class)))
+            .thenReturn(false);
+
+        when(accountRepository.save(any(Account.class)))
+            .thenReturn(actual);
+
+        Account expected = accountService
+            .create(
+                NewAccountDTO.builder()
+                    .name(actual.getName())
+                    .email(actual.getEmail())
+                    .phoneNumber(actual.getPhoneNumber())
+                    .build()
+            );
+
+        assertEquals(expected, actual);
+    }
+}
