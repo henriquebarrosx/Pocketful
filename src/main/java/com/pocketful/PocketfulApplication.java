48907326c4f7bd57389d471cf5dc53e45e0d7d42
Henrique Barros <henriquebarrosx@gmail.com>
2024-06-02 14:01:45 -0300

    install rabbitmq dependency

--- src/main/java/com/pocketful/PocketfulApplication.java
+++ src/main/java/com/pocketful/PocketfulApplication.java
@@ -1,8 +1,10 @@
 package com.pocketful;
 
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableRabbit
 @SpringBootApplication
 public class PocketfulApplication {
     public static void main(String[] args) {
