e67ec8aaeb48b93c2015973eb32f7b2a283c8a90
Henrique Barros <henriquebarrosx@gmail.com>
2024-07-03 16:38:28 -0300

    Add logs to producers e consumers

--- src/main/java/com/pocketful/consumer/PaymentEditionQueueConsumer.java
+++ src/main/java/com/pocketful/consumer/PaymentEditionQueueConsumer.java
@@ -3,12 +3,14 @@ package com.pocketful.consumer;
 import com.pocketful.service.PaymentService;
 import com.pocketful.web.dto.payment.PaymentEditionQueuePayload;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
 import static com.pocketful.config.RabbitMqConfig.PAYMENTS_EDITION_QUEUE;
 
+@Slf4j
 @AllArgsConstructor
 @Component
 public class PaymentEditionQueueConsumer {
@@ -16,6 +18,8 @@ public class PaymentEditionQueueConsumer {
 
     @RabbitListener(queues = PAYMENTS_EDITION_QUEUE)
     public void receive(@Payload PaymentEditionQueuePayload payload) {
+        log.info("PaymentEditionQueueConsumer.receive start payload: {}", payload);
         paymentService.processPaymentEdition(payload.getPayment(), payload.getType());
+        log.info("PaymentEditionQueueConsumer.receive end payload: {}", payload);
     }
 }
--- src/main/java/com/pocketful/consumer/PaymentGenerationQueueConsumer.java
+++ src/main/java/com/pocketful/consumer/PaymentGenerationQueueConsumer.java
@@ -3,12 +3,14 @@ package com.pocketful.consumer;
 import com.pocketful.entity.Payment;
 import com.pocketful.service.PaymentService;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
 import static com.pocketful.config.RabbitMqConfig.PAYMENTS_GENERATION_QUEUE;
 
+@Slf4j
 @AllArgsConstructor
 @Component
 public class PaymentGenerationQueueConsumer {
@@ -16,6 +18,8 @@ public class PaymentGenerationQueueConsumer {
 
     @RabbitListener(queues = PAYMENTS_GENERATION_QUEUE)
     public void receive(@Payload Payment payment) {
+        log.info("PaymentGenerationQueueConsumer.receive start payload: {}", payment);
         paymentService.processPaymentGeneration(payment);
+        log.info("PaymentGenerationQueueConsumer.receive end payload: {}", payment);
     }
 }
