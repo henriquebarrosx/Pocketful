5e703be9672399694642a215e6af6d23602d868b
Henrique Barros <henriquebarrosx@gmail.com>
2024-07-12 15:57:14 -0300

    Replace generic exceptions

--- src/main/java/com/pocketful/exception/InternalServerErrorException.java
+++
@@ -1,7 +0,0 @@
-package com.pocketful.exception;
-
-public class InternalServerErrorException extends RuntimeException {
-    public InternalServerErrorException(String message) {
-        super(message);
-    }
-}
