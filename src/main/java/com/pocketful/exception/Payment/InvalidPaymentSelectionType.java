92bcd79b4ebdedb1c2a229cbdb5e1bfb19bcbf14
Henrique Barros <henriquebarrosx@gmail.com>
2024-08-06 21:05:21 -0300

    Remove deprecated exception

--- src/main/java/com/pocketful/exception/Payment/InvalidPaymentSelectionType.java
+++
@@ -1,7 +0,0 @@
-package com.pocketful.exception.Payment;
-
-public class InvalidPaymentSelectionType extends RuntimeException {
-    public InvalidPaymentSelectionType() {
-        super("Invalid payment selection type");
-    }
-}
