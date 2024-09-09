8b1619a7c83cb4795d229acb34b3f39710972084
Henrique Barros <henriquebarrosx@gmail.com>
2024-09-08 21:15:45 -0300

    define classe de conexão com serviço rabbitmq

--- src/main/java/com/pocketful/config/RabbitMqConfig.java
+++ src/main/java/com/pocketful/config/RabbitMqConfig.java
@@ -2,6 +2,8 @@ package com.pocketful.config;
 
 import org.springframework.amqp.core.Declarable;
 import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -14,6 +16,27 @@ public class RabbitMqConfig {
 
     @Value("${queue.payments_edition_queue}")
     private String PAYMENTS_EDITION_QUEUE;
+
+    @Value("${rabbitmq.host}")
+    private String QUEUE_SERVICE_HOSTNAME;
+
+    @Value("${rabbitmq.port}")
+    private String QUEUE_SERVICE_PORT;
+
+    @Value("${rabbitmq.username}")
+    private String QUEUE_SERVICE_USERNAME;
+
+    @Value("${rabbitmq.password}")
+    private String QUEUE_SERVICE_PASSWORD;
+
+    @Bean
+    public ConnectionFactory connectionFactory() {
+        CachingConnectionFactory connectionFactory = new CachingConnectionFactory(QUEUE_SERVICE_HOSTNAME);
+        connectionFactory.setUsername(QUEUE_SERVICE_USERNAME);
+        connectionFactory.setPassword(QUEUE_SERVICE_PASSWORD);
+        connectionFactory.setPort(Integer.parseInt(QUEUE_SERVICE_PORT));
+        return connectionFactory;
+    }
 
     @Bean
     public Jackson2JsonMessageConverter producerJackson2MessageConverter() {
