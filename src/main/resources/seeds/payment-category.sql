26418ef1eacbd33c043fbe214c27ed5abafeb949
Henrique Barros <henriquebarrosx@gmail.com>
2024-09-17 21:48:20 -0300

    cria arquivo de seeds

---
+++ src/main/resources/seeds/payment-category.sql
@@ -0,0 +1,32 @@
+INSERT INTO payment_categories (id, name, created_at, updated_at)
+VALUES (1, 'Casa', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+
+INSERT INTO payment_categories (id, name, created_at, updated_at)
+VALUES (1, 'Educação', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+
+INSERT INTO payment_categories (id, name, created_at, updated_at)
+VALUES (2, 'Eletrônicos', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+
+INSERT INTO payment_categories (id, name, created_at, updated_at)
+VALUES (3, 'Lazer', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+
+INSERT INTO payment_categories (id, name, created_at, updated_at)
+VALUES (4, 'Restaurante', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+
+INSERT INTO payment_categories (id, name, created_at, updated_at)
+VALUES (5, 'Saúde', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+
+INSERT INTO payment_categories (id, name, created_at, updated_at)
+VALUES (6, 'Serviços', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+
+INSERT INTO payment_categories (id, name, created_at, updated_at)
+VALUES (7, 'Supermercado', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+
+INSERT INTO payment_categories (id, name, created_at, updated_at)
+VALUES (8, 'Transporte', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+
+INSERT INTO payment_categories (id, name, created_at, updated_at)
+VALUES (9, 'Vestuário', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+
+INSERT INTO payment_categories (id, name, created_at, updated_at)
+VALUES (10, 'Viagem', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
